60f35fe74136d30e4238b4921b48f2b000132b07
KDotIV <kylebsmall32@gmail.com>
2021-04-27 20:03:36 -0400

    Front-End Charts done

--- src/app/charts/line-chart/line-chart.component.ts
+++ src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartDataSets, ChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { LINE_CHART_COLORS} from '../../shared/chart.colors';
+
 
 @Component({
   selector: 'app-line-chart',
@@ -8,6 +12,29 @@ import { Component, OnInit } from '@angular/core';
 export class LineChartComponent implements OnInit {
 
   constructor() { }
+    // Array of different segments in chart
+    lineChartData: ChartDataSets[] = [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Product A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Product B' },
+      { data: [100, 59, 20, 67, 99, 12, 86], label: 'Product C'}
+    ];
+  
+    //Labels shown on the x-axis
+    lineChartLabels: Label[] = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+    // Define chart options
+    lineChartOptions: ChartOptions = {
+      responsive: true
+    };
+  
+    // Define colors of chart segments
+    lineChartColors = LINE_CHART_COLORS;
+  
+    // Set true to show legends
+    lineChartLegend = true;
+  
+    // Define type of chart
+    lineChartType = 'line';
 
   ngOnInit(): void {
   }
